0426c8d3ace8c6eb6c952cc95ebb9de9b3faf513
breno.alcantara <breno.alcantara@itsector.pt>
2025-04-06 19:27:11 +0100

    Initial Commit

---
+++ CrosswordBRTests/CrosswordBRTests.swift
@@ -0,0 +1,17 @@
+//
+//  CrosswordBRTests.swift
+//  CrosswordBRTests
+//
+//  Created by Breno Alcantara on 06/04/2025.
+//
+
+import Testing
+@testable import CrosswordBR
+
+struct CrosswordBRTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
